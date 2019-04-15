8d8a8ef2315c097b883f6958c5108d25b93acfb1
Kevin Garvey and Svitlana Filatova <svitlana@>
2019-04-15 16:38:38 -0700

    Add run player function

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,6 @@ class Player{
     this.alive = true;
     //Player Inventory
     this.weapon = "fists";
-    this.armor;
     this.items = [];
     //Player Position
     this.x = startPosX;
@@ -33,13 +32,28 @@ class Player{
   }
 }
 
+Player.prototype.getStats = function getStats(){
+  if (this.job === "warrior") {
+    this.maxHP = 20;
+    this.currentHP = 20;
+    this.strength = 4;
+    this.speed = 3;
+  } else if (this.job === "wizard"){
+    this.maxHP = 10;
+    this.currentHP = 10;
+    this.strength = 5;
+    this.speed = 4;
+  } else if (this.job === "thief"){
+    this.maxHP = 15;
+    this.currentHP = 15;
+    this.strength = 2;
+    this.speed = 5;
+  }
+}
+
 //Player explore functions
 Player.prototype.moveGrid = function moveGrid(){
-  if (combat === true) {
-    //roll to attempt to escape, otherwise can't move and lose turn
-  } else {
-    //regular movement here
-  }
+
 }
 
 Player.prototype.checkGrid = function checkGrid(){
@@ -48,7 +62,16 @@ Player.prototype.checkGrid = function checkGrid(){
 
 //Player combat functions
 Player.prototype.fight = function fight(){
-  return this.attack;
+  return this.strength;
+}
+
+Player.prototype.run = function run(){
+  var escapeRoll = this.speed + Math.floor((Math.random() * 5 ) + 1);
+  if (escapeRoll >= 8) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 Player.prototype.takeDamage = function takeDamage(damageTaken){
@@ -58,7 +81,7 @@ if (this.currentHP <= 0) {
   }
 }
 
-//Player general functions
+//Player item functions
 Player.prototype.checkItems = function checkItems(){
   $("#combat-log").append("Items:" + "<br>");
   this.items.forEach(function(item) {
@@ -71,26 +94,6 @@ Player.prototype.addItem = function addItem(itemToAdd) {
 }
 
 Player.prototype.useItem = function useItems(itemToUse){
-
-}
-
-Player.prototype.getStats = function getStats(){
-  if (this.job === "warrior") {
-    this.maxHP = 20;
-    this.currentHP = 20;
-    this.attack = 4;
-    this.speed = 2;
-  } else if (this.job === "wizard"){
-    this.maxHP = 10;
-    this.currentHP = 10;
-    this.attack = 5;
-    this.speed = 3;
-  } else if (this.job === "thief"){
-    this.maxHP = 15;
-    this.currentHP = 15;
-    this.attack = 2;
-    this.speed = 5;
-  }
 }
 
 //Enemy functions
@@ -107,23 +110,23 @@ Enemy.prototype.getStats = function getStats(){
   if (this.type === "Slime") {
     this.maxHP = 5;
     this.currentHP = 5;
-    this.attack = 1;
+    this.strength = 1;
     this.speed = 1;
   } else if (this.type === "Troll") {
     this.maxHP = 15;
     this.currentHP = 15;
-    this.attack = 5;
+    this.strength = 5;
     this.speed = 2;
   } else if (this.type === "Dragon") {
     this.maxHP = 20;
     this.currentHP = 20;
-    this.attack = 10;
+    this.strength = 10;
     this.speed = 4;
   }
 }
 
 Enemy.prototype.fight = function fight(){
-  return this.attack;
+  return this.strength;
 
 }
 
@@ -136,8 +139,8 @@ if (this.currentHP <= 0) {
 
 class Item{
   constructor(){
+  }
 }
-
 //Helper Functions
 function printFloor(floorObj){
   console.log('Printing out our floor Layout');
@@ -174,6 +177,7 @@ function checkForDeath(playerStatus, enemyStatus) {
 
 
 function combat() {
+  debugger
   $(".dungeon-UI").hide();
   $(".combat-UI").show();
   var newEnemy = new Enemy("Dragon");
@@ -188,7 +192,6 @@ function combat() {
   }
     $("#attack-combat").click(function(event) {
       event.preventDefault();
-      debugger
       var damageDone = newPlayer.fight();
       newEnemy.takeDamage(damageDone);
       $("#combat-log").append(newPlayer.name + " attacked with their " + newPlayer.weapon + "<br>" + newEnemy.type + " took " + damageDone + " damage " + "<br>");
@@ -199,24 +202,26 @@ function combat() {
       checkForDeath(newPlayer.alive, newEnemy.alive);
   });
 
-    $("#items").click(function(event) {
+    $("#items-combat").click(function(event) {
       event.preventDefault();
       newPlayer.checkItems();
-      newEnemy.attack();
+      newEnemy.strength();
     });
 
-    $("#run").click(function(event) {
+    $("#run-combat").click(function(event) {
+      debugger
       event.preventDefault();
-      newPlayer.run();
-      if (success = true) {
+      var success = newPlayer.run();
+      if (success === true) {
         $("#combat-log").append(newPlayer.name + " successfully ran away" + "<br>");
         //switch to dungeon phase
-      }
+      } else {
         $("#combat-log").append(newPlayer.name + " failed to run away" + "<br>");
         var damageTaken = newEnemy.fight();
         newPlayer.takeDamage(damageTaken);
         $("#combat-log").append(newEnemy.type + " attacked and did " + damageTaken + " damage" + "<br>");
         checkForDeath(newPlayer.alive, newEnemy.alive);
+      }
       });
   }
 
